c0167022c3a11ff3b777286d5acbd397b8d8f753
korujamscongo <korujamscongo@gmail.com>
2015-12-29 11:36:29 +0000

    Latest commit:

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,26 @@
 class OrdersController < ApplicationController
     def new
         @order = Order.new
+        render :show_form
     end
     def create
         @order = Order.create(order_params)
         @order.user_id = current_user.id
         @orders  = Order.all
+        render :hide_form
     end
+    
+    def edit 
+        @order = Order.edit(params[:id])
+        render :show_form
+    end
+    def update
+      @order = Order.find(params[:id])
+      @order.update_attributes(order_params)
+      
+    end
+        
+        
     def destroy
         @order = Order.find(params[:id])
         @order.destroy
@@ -15,7 +29,7 @@ class OrdersController < ApplicationController
     end
     private
     def order_params
-        params.require(:order).permit(:department_id,:type_id,:problem,:user_id)
+        params.require(:order).permit(:department_id,:type_id,:problem,:user_id , :completed)
         
     end
 end
